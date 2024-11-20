2a197bda73943d19d298e7708cfb32d34b2a0389
Sun Mingfu <mingfu.sun1@epfedu.fr>
2024-11-20 10:54:05 +0100

    say hello

--- LightOff_MINGFU_version_console/src/lightoff_mingfu_version_console/CelluleLumineuse.java
+++ LightOff_MINGFU_version_console/src/lightoff_mingfu_version_console/CelluleLumineuse.java
@@ -10,4 +10,42 @@ package lightoff_mingfu_version_console;
  */
 public class CelluleLumineuse {
     
+    private boolean etat;
+
+    public CelluleLumineuse() {
+        this.etat = false;
+    }
+    
+    public void activerCellule() {
+        this.etat = !this.etat;
+    }
+    
+    public void eteindreCellule() {
+        this.etat = false;
+    }
+
+    public boolean estEteint() {
+        return !this.etat;
+    }
+
+    public boolean getEtat() {
+        return this.etat;
+    }
+
+    @Override
+    public String toString() {
+        return this.etat ? "X" : "O";
+    }
+
+    public static void main(String[] args) {
+        CelluleLumineuse cellule = new CelluleLumineuse();
+        System.out.println(cellule); 
+        
+        cellule.activerCellule();
+        System.out.println(cellule); 
+        
+        cellule.eteindreCellule();
+        System.out.println(cellule);
+    }
+
 }
