6ca327bc9a752a41d4bee3eaeb2a3f5b0d5bfcf8
Sun Mingfu <mingfu.sun1@epfedu.fr>
2024-11-20 10:34:36 +0100

    hello

--- LightOff_MINGFU_version_console/src/lightoff_mingfu_version_console/LightOff_MINGFU_version_console.java
+++ LightOff_MINGFU_version_console/src/lightoff_mingfu_version_console/LightOff_MINGFU_version_console.java
@@ -16,6 +16,7 @@ public class LightOff_MINGFU_version_console {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello");
+        System.out.println("hello");
     }
     
 }
