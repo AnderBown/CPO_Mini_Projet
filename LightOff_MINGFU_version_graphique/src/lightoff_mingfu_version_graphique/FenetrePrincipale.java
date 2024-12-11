e5c2307bff22ef6f71555d7fe776d2d59b675fe4
Sun Mingfu <mingfu.sun1@epfedu.fr>
2024-12-11 12:26:43 +0100

    comfirme boutton

--- LightOff_MINGFU_version_graphique/src/lightoff_mingfu_version_graphique/FenetrePrincipale.java
+++ LightOff_MINGFU_version_graphique/src/lightoff_mingfu_version_graphique/FenetrePrincipale.java
@@ -149,8 +149,6 @@ public class FenetrePrincipale extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
-        Difficulty = new javax.swing.JTextField();
-        jLabel4 = new javax.swing.JLabel();
         labelNbCoups = new javax.swing.JLabel();
         PanneauGrille = new javax.swing.JPanel();
 
@@ -165,6 +163,7 @@ public class FenetrePrincipale extends javax.swing.JFrame {
 
         jLabel1.setBackground(new java.awt.Color(51, 255, 0));
         jLabel1.setText("                                                                                                 LIGHTS OFF");
+        jLabel1.setPreferredSize(new java.awt.Dimension(400, 30));
         getContentPane().add(jLabel1, java.awt.BorderLayout.PAGE_START);
 
         jPanel1.setBackground(new java.awt.Color(153, 255, 255));
@@ -172,20 +171,10 @@ public class FenetrePrincipale extends javax.swing.JFrame {
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel2.setText("Nombre de tours");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 50, -1, -1));
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(15, 260, -1, -1));
 
-        Difficulty.setMinimumSize(new java.awt.Dimension(100, 100));
-        Difficulty.setPreferredSize(new java.awt.Dimension(100, 50));
-        Difficulty.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                DifficultyActionPerformed(evt);
-            }
-        });
-        jPanel1.add(Difficulty, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 300, -1, -1));
-
-        jLabel4.setText("Randomizer");
-        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 270, -1, -1));
-        jPanel1.add(labelNbCoups, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 80, -1, -1));
+        labelNbCoups.setText("1");
+        jPanel1.add(labelNbCoups, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 290, -1, -1));
 
         PanneauGrille.setBackground(new java.awt.Color(51, 204, 255));
         PanneauGrille.setPreferredSize(new java.awt.Dimension(600, 600));
@@ -211,18 +200,6 @@ public class FenetrePrincipale extends javax.swing.JFrame {
     private void btnLigne0ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLigne0ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_btnLigne0ActionPerformed
-
-    private void DifficultyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DifficultyActionPerformed
-        // Get the text from the input field (assumed to be named difficultyField)
-        String inputText = Difficulty.getText();  // Corrected line
-
-        try {
-            int difficulty = Integer.parseInt(inputText); // Convert the input to an integer
-            grille.melangerMatriceAleatoirement(difficulty); // Pass the difficulty to the method
-        } catch (NumberFormatException ex) {
-            JOptionPane.showMessageDialog(null, "Veuillez entrer un nombre valide.");
-        }
-    }//GEN-LAST:event_DifficultyActionPerformed
 
     /**
      * @param args the command line arguments
@@ -260,12 +237,10 @@ public class FenetrePrincipale extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextField Difficulty;
     private javax.swing.JPanel PanneauGrille;
     private javax.swing.JButton btnLigne0;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel labelNbCoups;
     // End of variables declaration//GEN-END:variables
